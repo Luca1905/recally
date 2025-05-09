75585992bb2949a51f3adee653baebb1a12549c4
Luca1905 <contact@lucawang.me>
2025-05-09 22:29:19 +0200

    FIX: env validation during build

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import {fileURLToPath} from "node:url";
-import createJiti from "jiti";
 import "./src/env.js";
-
-const jiti= createJiti(fileURLToPath(import.meta.url));
- 
-jiti("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {};
