800dac11069a06d5e8808c74d42c3918822b662a
Luca1905 <contact@lucawang.me>
2025-06-04 16:37:16 +0200

    FIX: sidebar height

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,7 +13,7 @@ export default function AppLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar className="h-full" />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
