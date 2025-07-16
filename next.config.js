ffcd9c1a6cb129d7066c85b1a5ffdc6f0d7cb3c8
Luca Wang <contact@lucawang.me>
2025-07-16 19:09:48 +0200

    integrated posthog

--- next.config.js
+++ next.config.js
@@ -13,6 +13,27 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  // Rewrites to support PostHog
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://eu.i.posthog.com/decide",
+      },
+    ];
+  },
+
+  // Required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default config;
