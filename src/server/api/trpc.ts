3f703e5cc7f7f7677ead3e15e8b3b30dc849f77b
Luca Wang <contact@lucawang.me>
2025-07-16 21:41:20 +0200

    FIX: added redis to trpc context

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -11,6 +11,7 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { db } from "~/server/db";
+import { redis as cache } from "~/server/cache/redis"
 
 /**
  * 1. CONTEXT
@@ -27,6 +28,7 @@ import { db } from "~/server/db";
 export const createTRPCContext = async (opts: { headers: Headers }) => {
   return {
     db,
+    cache,
     ...opts,
   };
 };
